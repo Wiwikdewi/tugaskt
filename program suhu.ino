5b1f540dc65645dc2f2d502da76f10227aecbeb0
Wiwikdewi <wiwikdewiyulistia@gmail.com>
2016-12-11 05:13:44 -0800

    Add files via upload

---
+++ program suhu.ino
@@ -0,0 +1,20 @@
+
+int PinSuhu = A0 ;
+float suhu, data ;
+void setup() {
+Serial.begin(9600) ;
+pinMode(PinSuhu, INPUT) ;
+}
+void loop() {
+  data = analogRead(PinSuhu) ;
+  suhu = data * 500 / 1024 ;
+   
+  Serial.print("data : ") ;
+  Serial.print(data ) ;
+  Serial.print("  Suhu : ");
+  Serial.print(suhu) ;
+  Serial.print(" C") ;
+  Serial.println();
+  delay(1000) ;
+}
+
